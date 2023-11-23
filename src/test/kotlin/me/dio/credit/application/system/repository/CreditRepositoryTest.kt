c7050d69f6888a6efa301dae51ae39e129883500
WilliamElesbao <william.elesbao.2000@gmail.com>
2023-11-23 16:00:28 -0300

    teste de integracao: persistindo dados com TestEntityManager

---
+++ src/test/kotlin/me/dio/credit/application/system/repository/CreditRepositoryTest.kt
@@ -0,0 +1,66 @@
+package me.dio.credit.application.system.repository
+
+import me.dio.credit.application.system.entity.Address
+import me.dio.credit.application.system.entity.Credit
+import me.dio.credit.application.system.entity.Customer
+import org.junit.jupiter.api.BeforeEach
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.Month
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditRepositoryTest {
+    @Autowired lateinit var creditRepository: CreditRepository
+    @Autowired lateinit var testEntityManager: TestEntityManager
+
+    private lateinit var customer: Customer
+    private lateinit var credit1: Credit
+    private lateinit var credit2: Credit
+
+    @BeforeEach fun setup(){
+        customer = testEntityManager.persist(buildCustomer())
+        credit1 = testEntityManager.persist(buildCredit(customer = customer))
+        credit2 = testEntityManager.persist(buildCredit(customer = customer))
+    }
+
+    private fun buildCredit(
+        creditValue: BigDecimal = BigDecimal.valueOf(500.00),
+        dayFirstInstallment: LocalDate = LocalDate.of(2023, Month.NOVEMBER, 23),
+        numberOfInstallments: Int = 5,
+        customer: Customer
+    ): Credit = Credit(
+        creditValue = creditValue,
+        dayFirstInstallment = dayFirstInstallment,
+        numberOfInstallment = numberOfInstallments,
+        customer = customer
+    )
+
+    private fun buildCustomer(
+        firstName: String = "William",
+        lastName: String = "Elesbão",
+        cpf: String = "05882648068",
+        email: String = "william@email.com",
+        password: String = "william123",
+        zipCode: String = "91030380",
+        street: String = "rua do william",
+        income: BigDecimal = BigDecimal.valueOf(2000.00)
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street
+        ),
+        income = income,
+    )
+}
--- src/test/kotlin/me/dio/credit/application/system/service/CreditServiceTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/service/CreditServiceTest.kt
@@ -1,12 +1,7 @@
 package me.dio.credit.application.system.service
 
-import me.dio.credit.application.system.repository.CustomerRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
-
 class CreditServiceTest {
-    @Autowired lateinit var customerRepository: CustomerRepository
-    @Autowired lateinit var testEntityManager: TestEntityManager
+
 
 
 }
