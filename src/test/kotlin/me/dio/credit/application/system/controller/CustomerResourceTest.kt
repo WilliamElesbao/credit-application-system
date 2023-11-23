cbf8fcaaa82036f298f7541de35f1e37a48a6e45
WilliamElesbao <william.elesbao.2000@gmail.com>
2023-11-23 17:51:56 -0300

    teste de integracao: CustomerResourceTest findById - parte 2

--- src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
+++ src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
@@ -120,6 +120,26 @@ fun `should find customer by id and return 200 status`(){
         .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1))
         .andDo(MockMvcResultHandlers.print())
 }
+
+    @Test
+    fun `should not find customer with invalid id and return 400 status`(){
+        //given
+        val invalidId: Long = 2L
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("$URL/$invalidId")
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isBadRequest)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Bad Request! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(400))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.exception")
+                .value("class me.dio.credit.application.system.exception.BusinessException"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+
     private fun buildCustomerDto(
         firstName: String = "William",
         lastName: String = "Elesbão",
